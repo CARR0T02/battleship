e8526eedc8072637782d8bc7c16e5d5d8c651867
CARR0T02 <mr.gabriel.chang@gmail.com>
2023-05-12 11:58:43 +0800

    Implement randomly placing ships

--- src/scripts/Game.js
+++ src/scripts/Game.js
@@ -3,18 +3,47 @@ import createShip from './Ship.js';
 
 let player1, player2, currPlayer, oppPlayer;
 
+let shipSizes = [2, 2, 3, 4];
+
 const newGame = function (name1, name2 = 'AI') {
   player1 = Player(name1);
   player2 = Player(name2);
   currPlayer = player1;
   oppPlayer = player2;
-  player1.board.placeHorizontal(0, createShip(3));
-  player1.board.placeVertical(48, createShip(2));
-  player2.board.placeHorizontal(0, createShip(3));
-  player2.board.placeVertical(48, createShip(2));
+  placeShips(player1);
+  placeShips(player2);
 };
 
-// Returns true when game has ended
+const placeShips = function (player) {
+  let orientation, index, response;
+  for (const shipSize of shipSizes) {
+    do {
+      orientation = Math.floor(Math.random() * 2);
+      index = Math.floor(Math.random() * player.board.size ** 2);
+      if (orientation === 0) {
+        response = player.board.placeHorizontal(index, createShip(shipSize));
+      } else if (orientation === 1) {
+        response = player.board.placeVertical(index, createShip(shipSize));
+      }
+    } while (!response);
+  }
+};
+
+const playRandom = function () {
+  let response = currPlayer.attackRandom(oppPlayer.board);
+  if (response === false) {
+    return response;
+  }
+  if (oppPlayer.board.isAlive()) {
+    changeTurn();
+    console.log(`${currPlayer.name}'s turn`);
+    return response;
+  } else {
+    return endGame();
+  }
+};
+
+// Returns true when game has ended, false if invalid move
 // Returns ship if hit and null if miss
 const playTurn = function (index) {
   let response = currPlayer.attack(index, oppPlayer.board);
@@ -44,9 +73,13 @@ const getCurrPlayer = function () {
   return currPlayer;
 };
 
+const getOppPlayer = function () {
+  return oppPlayer;
+};
+
 const endGame = function () {
   console.log(`The winner is ${currPlayer.name}!`);
   return true;
 };
 
-export { newGame, playTurn, getCurrPlayer };
+export { newGame, playTurn, playRandom, getCurrPlayer, getOppPlayer };
